b0e315fef7c130184b1a0fb9b26cb992900f9575
Quang Man <manvanquang1109@gmail.com>
2021-05-04 11:53:28 +0700

    stm32: THANH CONG gui lenh AT sang esp8266 (GET req len server heroku), nhan thanh cong du lieu

--- stm32/ESP8266.h
+++ stm32/ESP8266.h
@@ -5,8 +5,11 @@
 #include "main.h"
 #include "UART.h"
 
-#define WIFI_SSID	"MVMinh"
-#define	WIFI_PASS	"15061980"
+#define WIFI_SSID	"VIETTEL_2.4G_NP3DgK"
+#define	WIFI_PASS	"thichanga"
+
+#define URL				"http://esp8266-sendreq.herokuapp.com"
+#define GET_REQ		"GET / HTTP/1.1\r\nHost: esp8266-sendreq.herokuapp.com"
 
 void initESP(void);
 
